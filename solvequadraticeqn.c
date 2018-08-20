c2ae49bf57cb64bdd86569072358c2c5bc7ced11
Wibuprogrammer <38586802+Wibunerd@users.noreply.github.com>
2018-08-20 22:40:31 +0700

    Quadratic equation program
    1st project completed!

---
+++ solvequadraticeqn.c
@@ -0,0 +1,53 @@
+/* This is the program that solve quadratic equation
+  imaginary roots will not be included
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float a,b,c,d,root1,root2;
+
+  printf("Enter a, b and c in the form ax^2 + bx + c\n");
+
+  printf("Enter a\n");
+  
+  if(scanf("%f", &a)==0){
+    printf("Enter numbers only\n");
+  }else {
+    printf("Enter b\n");
+    
+    if(scanf("%f", &b)==0){
+      printf("Enter numbers only\n");
+    }else {
+      printf("Enter c\n");
+      
+      if(scanf("%f", &c)==0){
+        printf("Enter numbers only\n");
+    }else{
+      d = b*b-4*a*c;//calculate delta
+
+      if(a==0){
+         if(b==0){
+            printf("There are no roots");
+          }else{
+            root1 = -c/b;
+             printf("The equation has one root: %f", root1);
+             }
+      }else{
+        if(d<0){
+         printf("There are no real roots");
+         
+         }else if(d==0){
+            root1 = -b/(2*a);
+            printf("The equation has 2 equal roots: %f", root1);
+            }else{
+              root1 = (-b+sqrt(d))/(2*a);
+              root2 = (-b-sqrt(d))/(2*a);
+             printf("The equation has 2 roots:%f\t%f", root1, root2);
+             }
+          }
+        }
+      }
+    }
+ return 0;
+}
